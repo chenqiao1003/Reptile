3ada2bcd5cf63a873ec1d5d0e93dfb70ed1fa79f
laoyoutiao007 <chenqiao1003@126.com>
2019-07-16 17:28:13 +0800

    删除多余的注释
    删除多余注释

--- app/index.php
+++ app/index.php
@@ -16,31 +16,8 @@ $re = json_decode($data, true);
 
 $dataTmp = [];
 foreach ($re as $key => &$value) {
-    $value = str_replace('��', '', $value);
+    $value = str_replace('´ð£º', '', $value);
     $dataTmp[] = $value;
 }
 
 echo json_encode(['content' => $dataTmp]);
-
-// #searchAnswer dd{line-height: 30px;list-style: none;margin: 0 20px;padding: 10px 0;border-bottom: 1px dashed #ddd;cursor: pointer;}
-// <div id="searchAnswer" class="answer-list" style="display: none"></div>
-//getAnswer();
-//function getAnswer() {
-//    $.ajax({
-//                url      : 'http://zyf.jinbaosan.com/?id=' + <{$adminInfo.id}> + '&&tagsInfo=' + $("#searchTitle").html(),
-//                data     : {
-//    },
-//                dataType : 'json',
-//                type     : 'get',
-//                success  : function(response) {
-//        if (typeof (response.content) != 'undefined'){
-//            var html = '';
-//            for(var i=0; i<response.content.length; i++){
-//                html += response.content[i];
-//            }
-//                        $("#searchAnswer").html(html);
-//                        $("#searchAnswer").css('display', 'block');
-//                    }
-//    }
-//            });
-//}
